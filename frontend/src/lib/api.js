ca850eb35d4ae37600df5b28ca7f6a0deb63c223
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-18 01:25:29 +1100

    feat: Implement Milestone 1 api

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,73 @@
+import { ToastError } from "../components/ToastError.js";
+import { BACKEND_PORT } from "./config.js";
+
+const baseURL = `http://localhost:${BACKEND_PORT}`
+
+const register = (email, password, name) => {
+  return fetch(`${baseURL}/auth/register`, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      name: name
+    })
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.error) {
+        ToastError(data.error);
+        return Promise.reject(data);
+      } else {
+        localStorage.setItem('token', data.token);
+        return data;
+      }
+    })
+}
+
+const login = (email, password) => {
+  return fetch(`${baseURL}/auth/login`, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    })
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.error) {
+        ToastError(data.error);
+        return Promise.reject(data);
+      } else {
+        localStorage.setItem('token', data.token);
+        return data;
+      }
+    })
+}
+
+const logout = () => {
+  return fetch(`${baseURL}/auth/logout`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+  })
+    .then(res => res.json())
+    .then((data => {
+      if (data.error) {
+        ToastError(data.error);
+        localStorage.removeItem('token');
+        return Promise.reject(data);
+      } else {
+        localStorage.removeItem('token');
+      }
+    }))
+}
+
+export {
+  register,
+  login,
+  logout,
+}

8c79c99a1a0d9f054828daac9fd4f9833caef3ec
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-20 02:23:57 +1100

    feat: Implemented create channel modal

---
+++ frontend/src/components/CreateChannelModal.js
@@ -0,0 +1,31 @@
+import { createChannel } from "../lib/api.js";
+
+export const CreateChannelModal = (doGetChannels) => {
+  const mountpoint = document.getElementById('modal-container');
+  const modal = document.getElementById('create-channel-modal-component').content.cloneNode(true);
+  mountpoint.replaceChildren(modal);
+  mountpoint.showModal();
+
+  const name = document.getElementById('create-channel-name');
+  const description = document.getElementById('create-channel-description');
+  const isPrivate = document.getElementById('create-channel-is-private');
+  const cancelBtn = document.getElementById('create-channel-cancel');
+  const form = mountpoint.querySelector('form');
+
+  cancelBtn.addEventListener('click', () => mountpoint.close());
+
+  name.addEventListener('blur', () => {
+    if (!name.checkValidity()) {
+      name.classList.add('input-error');
+    } else {
+      name.classList.remove('input-error');
+    }
+  });
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    createChannel(name.value, isPrivate.checked, description.value)
+      .then(doGetChannels());
+    mountpoint.close();
+  });
+}

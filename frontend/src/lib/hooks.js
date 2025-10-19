4ee2b8a49a0732b322aa0cade3689adf8b3512a7
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-19 22:16:13 +1100

    feat: Implement useState hook

---
+++ frontend/src/lib/hooks.js
@@ -0,0 +1,29 @@
+const useState = (initialValue) => {
+  let value = initialValue;
+  const listeners = new Set();
+
+  const getter = () => value;
+  const setter = (newValue) => {
+    const updatedValue = typeof newValue === 'function'
+    ? newValue(value)
+    : newValue;
+
+    if (updatedValue === value) return;
+    value = updatedValue;
+    listeners.forEach(callback => callback(value));
+  };
+
+  const subscribe = (callback) => {
+    listeners.add(callback);
+
+    return () => listeners.delete(callback);
+  };
+
+  const logListeners = () => console.log(listeners);
+
+  return [getter, setter, subscribe, logListeners]
+}
+
+export {
+  useState
+}

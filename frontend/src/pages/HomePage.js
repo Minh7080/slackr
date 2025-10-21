91c179c20976db47105c88ac971cb3911e287c52
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-22 04:11:49 +1100

    feat: Update homepage to show something when no channel selected

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,3 +1,5 @@
+import { MessageDashboard } from "../components/MessageDashboard.js";
+import { MessageDashboardWarning } from "../components/MessageDashboardWarning.js";
 import { Sidebar } from "../components/Sidebar.js";
 import { logout } from "../lib/api.js";
 import { useState } from "../lib/hooks.js";
@@ -8,12 +10,26 @@ export const HomePage = () => {
   const page = document.getElementById('home-page').content.cloneNode(true);
   main.replaceChildren(page);
 
+  const [getSelectedChannel, setSelectedChannel, subSelectedChannel] = useState(null);
+  Sidebar(setSelectedChannel, subSelectedChannel, getSelectedChannel);
+
+  const updateDashboard = (selectedChannel) => {
+    if (!selectedChannel) {
+      MessageDashboardWarning();
+      return;
+    }
+    MessageDashboard(subSelectedChannel, getSelectedChannel);
+  }
+
+  subSelectedChannel(selectedChannel => {
+    updateDashboard(selectedChannel);
+    console.log(selectedChannel);
+  });
+
+  updateDashboard(getSelectedChannel());
+
   const logoutBtn = document.getElementById('logout-button');
   logoutBtn.addEventListener('click', () => {
     logout().finally(() => LoginPage());
   });
-
-  const [getSelectedChannel, setSelectedChannel, subSelectedChannel] = useState(null);
-
-  Sidebar(setSelectedChannel, subSelectedChannel);
 }

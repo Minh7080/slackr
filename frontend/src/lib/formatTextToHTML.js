6941072e5b39a37f5144041af6b5dc434452ec34
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-22 04:12:58 +1100

    feat: Implemented formatHTML helper

---
+++ frontend/src/lib/formatTextToHTML.js
@@ -0,0 +1,14 @@
+export const formatTextToHTML = (text) => {
+  return createParagraph(text);
+}
+
+const createParagraph = (text) => {
+  const fragment = document.createDocumentFragment();
+
+  for (const line of text.split('\n')) {
+    const p = document.createElement('p');
+    p.textContent = line;
+    fragment.appendChild(p);
+  }
+  return fragment;
+}

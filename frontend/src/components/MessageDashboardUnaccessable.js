582527c4afc39f1a0e046e50887eaf47b40b90c0
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-22 20:55:21 +1100

    feat: Implement inacessable component

---
+++ frontend/src/components/MessageDashboardUnaccessable.js
@@ -0,0 +1,33 @@
+import { getChannels, joinChannel } from "../lib/api.js";
+
+const unsubscribers = [];
+export const MessageDashboardUnaccessable = ({ setChannels, subChannels, getSelectedChannelId }) => {
+  unsubscribers.forEach(unsub => unsub());
+
+  const mounpoint = document.getElementById('message-dashboard-mountpoint');
+  const dashboardWarning = document.getElementById('message-dashboard-unaccessable-component').content.cloneNode(true);
+  mounpoint.replaceChildren(dashboardWarning);
+
+  const channelName = document.getElementById('message-dashboard-unaccessable-name');
+  const joinBtn = document.getElementById('message-dashboard-unaccessable-join-button');
+
+  const updateTitle = (data) => {
+    channelName.textContent = data.find(x => x.id === getSelectedChannelId()).name;
+  }
+
+  unsubscribers.push(subChannels(data => {
+    updateTitle(data);
+  }));
+
+  getChannels().then(x => updateTitle(x));
+
+  joinBtn.addEventListener('click', () => {
+    joinChannel(getSelectedChannelId())
+      .then(() => {
+        getChannels().then(data => {
+          setChannels(data);
+        })
+      })
+  });
+
+}

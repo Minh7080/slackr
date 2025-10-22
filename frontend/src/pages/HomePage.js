29cbea5d856c3afa2bcfec08f73c7dba731ee73e
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-22 20:56:16 +1100

    feat: Homepage decide to show which screen

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,8 @@
 import { MessageDashboard } from "../components/MessageDashboard.js";
+import { MessageDashboardUnaccessable } from "../components/MessageDashboardUnaccessable.js";
 import { MessageDashboardWarning } from "../components/MessageDashboardWarning.js";
 import { Sidebar } from "../components/Sidebar.js";
-import { logout } from "../lib/api.js";
+import { getChannels as getChannelsAPI, logout } from "../lib/api.js";
 import { useState } from "../lib/hooks.js";
 import { LoginPage } from "./LoginPage.js";
 
@@ -10,23 +11,39 @@ export const HomePage = () => {
   const page = document.getElementById('home-page').content.cloneNode(true);
   main.replaceChildren(page);
 
-  const [getSelectedChannel, setSelectedChannel, subSelectedChannel] = useState(null);
-  Sidebar(setSelectedChannel, subSelectedChannel, getSelectedChannel);
+  const [getSelectedChannelId, setSelectedChannelId, subSelectedChannelId] = useState(-1);
+  const [getChannels, setChannels, subChannels] = useState([]);
 
-  const updateDashboard = (selectedChannel) => {
-    if (!selectedChannel) {
+  getChannelsAPI().then(data => {
+    setChannels(data);
+  });
+
+  Sidebar({ setSelectedChannelId, getSelectedChannelId ,subSelectedChannelId, subChannels, getChannels, setChannels });
+
+  const updateDashboard = (selectedChannelId) => {
+    if (selectedChannelId === -1) {
       MessageDashboardWarning();
       return;
     }
-    MessageDashboard(subSelectedChannel, getSelectedChannel);
+
+    if (!getChannels().find(x => x.id === getSelectedChannelId()).members.includes(parseInt(localStorage.getItem('userId')))) {
+      MessageDashboardUnaccessable({ setChannels, subChannels, getSelectedChannelId });
+      return;
+    }
+
+    MessageDashboard({ subSelectedChannelId, getChannels, subChannels, getSelectedChannelId, setChannels });
+
   }
 
-  subSelectedChannel(selectedChannel => {
-    updateDashboard(selectedChannel);
-    console.log(selectedChannel);
+  subSelectedChannelId(selectedChannelId => {
+    updateDashboard(selectedChannelId);
   });
 
-  updateDashboard(getSelectedChannel());
+  subChannels(() => {
+    updateDashboard(getSelectedChannelId());
+  });
+
+  updateDashboard(getSelectedChannelId());
 
   const logoutBtn = document.getElementById('logout-button');
   logoutBtn.addEventListener('click', () => {

6de70fa3f57aaaf7c9a07dcfae1981b5eafc2195
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-27 18:22:44 +1100

    feat: Implement pinned message logic

---
+++ frontend/src/components/PinnedMessage.js
@@ -0,0 +1,37 @@
+import { getUserDetails } from '../lib/api.js';
+import { dateFormatter } from '../lib/dateFormatter.js';
+import { formatTextToHTML } from '../lib/formatTextToHTML.js';
+import { MessageReactLabels } from './MessageReactLabels.js';
+
+export const PinnedMessage = ({ message }) => {
+  const messageTemplate = document.getElementById('pinned-message-component').content.cloneNode(true);
+  const messageElement = messageTemplate.firstElementChild;
+
+  const usernameElement = messageElement.querySelector('.message-username');
+  const contentElement = messageElement.querySelector('.message-content');
+  const avatarElement = messageElement.querySelector('.message-avatar');
+  const timestamp = messageElement.querySelector('.message-timestamp');
+  const reactLabelsMountpoint = messageElement.querySelector('.message-react-labels-mountpoint');
+  const editTimestamp = messageElement.querySelector('.message-edit-timestamp');
+
+
+  const updateMessageDOM = (currentMessage = message) => {
+    contentElement.replaceChildren(formatTextToHTML(currentMessage.message));
+    timestamp.textContent = dateFormatter(currentMessage.sentAt);
+    if (currentMessage.edited) {
+      editTimestamp.textContent = `(edited ${dateFormatter(currentMessage.editedAt)})`;
+      editTimestamp.classList.remove('hidden');
+    }
+    reactLabelsMountpoint.replaceChildren(MessageReactLabels({ reacts: message.reacts, reactFn: () => {} }));
+  }
+
+  updateMessageDOM(message);
+
+  setInterval(() => updateMessageDOM(message), 5000);
+
+  return getUserDetails(message.sender).then(data => {
+    usernameElement.textContent = data.name;
+    avatarElement.src = data.image ? data.image : '../../assets/avatar.svg';
+    return messageElement;
+  });
+};

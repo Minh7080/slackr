f127929c01c606fcb00b4dc318a8c1eaa12f6a51
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-28 00:39:48 +1100

    feat: Implement InviteProfile

---
+++ frontend/src/components/InviteProfile.js
@@ -0,0 +1,30 @@
+import { getUserDetails } from '../lib/api.js';
+
+export const InviteProfile = ({ userId, selectedUserIdsSet }) => {
+  const profileTemplate = document.getElementById('invite-profile-component').content.cloneNode(true);
+  const profileElement = profileTemplate.firstElementChild;
+
+  const name = profileElement.querySelector('.invite-member-name');
+  const email = profileElement.querySelector('.invite-member-email');
+  const img = profileElement.querySelector('img');
+  const checkbox = profileElement.querySelector('input');
+
+  return getUserDetails(userId).then(data => {
+    checkbox.id = `invite-profile-${userId}`;
+    profileElement.htmlFor = checkbox.id;
+
+    checkbox.addEventListener('change', e => {
+      if (e.target.checked) {
+        selectedUserIdsSet.add(userId);
+      } else {
+        selectedUserIdsSet.delete(userId);
+      }
+    });
+
+    name.textContent = data.name;
+    email.textContent = data.email;
+    img.src = data.image ? data.image : 'assets/avatar.svg';
+    img.alt = `${data.name} profile picture`
+    return profileElement;
+  });
+};

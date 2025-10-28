db8b5d96162ad703baff65db111d45e92c2c7fda
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-29 03:15:51 +1100

    feat: Attack ProfileEditModal

--- frontend/src/components/ProfileModal.js
+++ frontend/src/components/ProfileModal.js
@@ -1,4 +1,6 @@
 import { getUserDetails } from '../lib/api.js';
+import { formatTextToHTML } from '../lib/formatTextToHTML.js';
+import { ProfileEditModal } from './ProfileEditModal.js';
 
 export const ProfileModal = ({ userId }) => {
   const mountpoint = document.getElementById('modal-container');
@@ -11,6 +13,7 @@ export const ProfileModal = ({ userId }) => {
   const profileEmail = document.getElementById('profile-email');
   const profileBio = document.getElementById('profile-bio');
   const profileBioHeading = document.getElementById('profile-bio-heading');
+  const editBtn = document.getElementById('profile-edit-button');
 
   cancelBtn.addEventListener('click', () => mountpoint.close());
 
@@ -18,14 +21,28 @@ export const ProfileModal = ({ userId }) => {
     profileEmail.textContent = user.email;
     profileName.textContent = user.name;
     profileImg.src = user.image ? user.image : 'assets/avatar.svg';
+
+    editBtn.addEventListener('click', () => { 
+      mountpoint.close();
+      ProfileEditModal({ user: user });
+    });
+
     if (user.bio) {
       profileBio.classList.remove('hidden');
       profileBioHeading.classList.remove('hidden');
-      profileBio.textContent = user.bio;
+      profileBio.replaceChildren(formatTextToHTML(user.bio));
     } else {
       profileBio.classList.add('hidden');
       profileBioHeading.classList.add('hidden');
     }
+
+    const currentUserId = parseInt(localStorage.getItem('userId'));
+    if (userId === currentUserId) {
+      editBtn.classList.remove('hidden');
+    } else {
+      editBtn.classList.add('hidden');
+    }
+
     mountpoint.showModal();
   });
 

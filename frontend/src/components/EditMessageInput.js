ac20a8b783b303aed3cff5711c00c31eb213917a
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-30 14:41:42 +1100

    feat: Made EditMessageInput reactive for images

--- frontend/src/components/EditMessageInput.js
+++ frontend/src/components/EditMessageInput.js
@@ -69,6 +69,50 @@ export const EditMessageInput = ({ getSelectedChannelId, message, updateMessageD
     updateImageButton();
   });
 
+  addImageBtn.addEventListener('click', () => imageInput.click());
+
+  const updateImagePreview = () => {
+    if (imageInput.files[0]) {
+      fileToDataUrl(imageInput.files[0]).then(url => {
+        imagePreview.querySelector('img').src = url;
+        inputElement.style.display = 'none';
+        imagePreview.style.display = '';
+        imageRemoved = false;
+        updateSubmitBtn();
+      });
+    } else if (message.image && !imageRemoved) {
+      // Show original image if no new file selected and not removed
+      imagePreview.querySelector('img').src = message.image;
+      inputElement.style.display = 'none';
+      imagePreview.style.display = '';
+      updateSubmitBtn();
+    } else {
+      // No image to show
+      inputElement.style.display = '';
+      imagePreview.style.display = 'none';
+      updateSubmitBtn();
+    }
+  }
+
+  updateImageButton();
+  updateImagePreview();
+  updateSubmitBtn();
+
+  imageInput.addEventListener('change', () => {
+    imageRemoved = false;
+    updateImagePreview();
+    updateImageButton();
+    updateSubmitBtn();
+  });
+
+
+  imagePreview.querySelector('button').addEventListener('click', () => {
+    imageInput.value = '';
+    imageRemoved = true;
+    updateImagePreview();
+    updateSubmitBtn();
+  })
+
   const submitMessage = () => {
     editMessage(getSelectedChannelId(), message.id, inputElement.value)
       .then(() => {
@@ -87,9 +131,10 @@ export const EditMessageInput = ({ getSelectedChannelId, message, updateMessageD
     submitMessage();
   });
 
-  inputElement.addEventListener('keydown', e => {
+  [addImageBtn, inputElement].forEach(element => element.addEventListener('keydown', e => {
     if (e.key === 'Enter' && !e.shiftKey && !submitBtn.disabled) {
-      submitMessage()
+      e.preventDefault();
+      submitMessage();
     }
-  })
+  }))
 };

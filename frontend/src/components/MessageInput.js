a78b5ada0208248185650eb92b64678beefbd1cb
Minh7080 <minhtuannguyen642005@gmail.com>
2025-10-30 14:38:37 +1100

    feat: MessageInput now can submit image

--- frontend/src/components/MessageInput.js
+++ frontend/src/components/MessageInput.js
@@ -1,4 +1,5 @@
-import { getMessages, sendMessage } from '../lib/api.js';
+import { getMessages, sendMessage, sendMessageImage } from '../lib/api.js';
+import { fileToDataUrl } from '../lib/imageToUrl.js';
 import { Message } from './Message.js';
 
 export const MessageInput = ({ getSelectedChannelId, loadPinnedMessages }) => {
@@ -11,18 +12,75 @@ export const MessageInput = ({ getSelectedChannelId, loadPinnedMessages }) => {
   const submitBtn = document.getElementById('message-submit-button');
   submitBtn.disabled = true;
 
+  const addImageBtn = document.getElementById('message-input-image-button');
+  const imageInput = document.getElementById('message-input-image-input');
+  const imagePreview = document.getElementById('message-input-image-preview');
+
+  const updateSubmitBtn = () => {
+    submitBtn.disabled = inputElement.value.trim() === "" && !imageInput.files[0];
+  }
+
   inputElement.addEventListener('input', () => {
     inputElement.style.height = 'auto';
     inputElement.style.height = inputElement.scrollHeight + 'px';
-    submitBtn.disabled = inputElement.value.trim() === "";
+    updateSubmitBtn();
   });
 
+  const updateImageButton = () => {
+    if (inputElement.value.trim()) {
+      addImageBtn.disabled = true;
+    } else {
+      addImageBtn.disabled = false;
+    }
+  }
+
+  inputElement.addEventListener('input', () => {
+    updateImageButton();
+  });
+
+  addImageBtn.addEventListener('click', () => imageInput.click());
+
+  const updateImagePreview = () => {
+    if (imageInput.files[0]) {
+      fileToDataUrl(imageInput.files[0]).then(url => {
+        imagePreview.querySelector('img').src = url;
+        inputElement.style.display = 'none';
+        imagePreview.style.display = '';
+      });
+    } else {
+      inputElement.style.display = '';
+      imagePreview.style.display = 'none';
+    }
+  }
+
+  imagePreview.style.display = 'none';
+  imageInput.addEventListener('change', () => {
+    updateImagePreview();
+    updateImageButton();
+    updateSubmitBtn();
+  });
+
+
+  imagePreview.querySelector('button').addEventListener('click', () => {
+    imageInput.value = '';
+    updateImagePreview();
+    updateSubmitBtn();
+  })
+
   const submitMessage = () => {
-    sendMessage(getSelectedChannelId(), inputElement.value)
+    const messagePromise = inputElement.value
+    ? sendMessage(getSelectedChannelId(), inputElement.value)
+    : fileToDataUrl(imageInput.files[0])
+      .then(url => sendMessageImage(getSelectedChannelId(), url));
+
+    messagePromise
       .then(() => {
-        inputElement.value = "";
+        inputElement.value = '';
+        imageInput.value = ''
         submitBtn.disabled = true;
         inputElement.style.height = 'auto';
+        updateImagePreview();
+        updateImageButton();
       })
       .then(() => {
         return getMessages(getSelectedChannelId(), 0);
@@ -34,15 +92,16 @@ export const MessageInput = ({ getSelectedChannelId, loadPinnedMessages }) => {
         messageMountpoint.appendChild(message);
         message.scrollIntoView({ behavior: 'smooth', block: 'end' });
       });
-  }
+  };
 
   submitBtn.addEventListener('click', () => {
     submitMessage();
   });
 
-  inputElement.addEventListener('keydown', e => {
+  [addImageBtn, inputElement].forEach(element => element.addEventListener('keydown', e => {
     if (e.key === 'Enter' && !e.shiftKey && !submitBtn.disabled) {
-      submitMessage()
+      e.preventDefault();
+      submitMessage();
     }
-  })
+  }))
 };

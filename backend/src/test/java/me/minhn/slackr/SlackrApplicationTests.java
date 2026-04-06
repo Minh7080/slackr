2dd9388d4607f74c300a2415f31c19f0b267c837
Minh7080 <minhtuannguyen642005@gmail.com>
2026-04-06 20:18:23 +1000

    feat: Initalise java backend

---
+++ backend/src/test/java/me/minhn/slackr/SlackrApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minhn.slackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
